6985faf9e73a7e92620cb0a3898fc66dea7b424f
Gleb enthru <enthru@enthru.net>
2020-12-07 13:53:32 +0300

    fixes

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -2,7 +2,7 @@
 
 # Initialize Kubernetes
 echo "[TASK 1] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=10.10.10.100 --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --apiserver-advertise-address=10.10.10.100 --pod-network-cidr=10.224.0.0/16 >> /root/kubeinit.log 2>/dev/null
 
 # Copy Kube admin config
 echo "[TASK 2] Copy kube admin config to Vagrant user .kube directory"
@@ -10,17 +10,17 @@ mkdir /home/vagrant/.kube
 cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown -R vagrant:vagrant /home/vagrant/.kube
 
+echo "[TASK 3] Deploy Calico network"
+su - vagrant -c "kubectl create -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml"
+
 # Generate Cluster join command
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > /joincluster.sh
 
 echo "[TASK 5] Deploy other services"
 yum -y install unzip git
-cd /vagrant
-git clone https://github.com/enthru/vagrant_k8s.git
-# Deploy flannel network
-su - vagrant -c "kubectl create -f /vagrant/vagrant_k8s/kube-flannel.yml"
-cd /home/vagrant
 git clone https://github.com/enthru/k8s_test.git
 cd k8s_test
-
+su - vagrant -c "kubectl apply -f service/zookeeper-svc.yaml"
+su - vagrant -c "kubectl apply -f statefulset/zookeeper-statefulset.yaml"
+sleep 300
